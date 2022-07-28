b95001342ca1ebc08b40613486d9a5e96dc12998
lemstanislav5 <lemstanislav5@gmail.com>
2022-07-28 18:56:33 +0300

    start change

--- src/componsnts/LoginPage/LoginPage.jsx
+++ src/componsnts/LoginPage/LoginPage.jsx
@@ -1,54 +1,56 @@
-import React from "react"
+import React, { useState } from 'react'
 import { Formik, Field, Form, ErrorMessage } from "formik"
 import styles from './LoginPage.module.css'
 import logo from '../../assets/img/logo.png'
+import { connect } from 'redux'
 
 const LoginPage = () => {
+
+  const [state, setState] = useState({
+    login: "", password: ""
+  });
+
+
+  // const validate = () => {
+  //   const errors = {};
+  //   if (!values.email) {
+  //     errors.email = 'Почта не указана'
+  //   } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+  //     errors.email = 'Некорректная почта'
+  //   }
+  //   if(!values.password) {
+  //     errors.password = 'Пароль не указан'
+  //   }
+  //   return errors;
+  // }
+
+
   return (
     <div className={styles.form_signin}>
       <div>
-     <Formik
-       initialValues={{ email: '', password: '' }}
-       validate={values => {
-          const errors = {};
-          if (!values.email) {
-            errors.email = 'Почта не указана'
-          } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
-            errors.email = 'Некорректная почта'
-          } 
-          if(!values.password) {
-            errors.password = 'Пароль не указан'
-          } 
-          return errors;
-      }
-       }
-       onSubmit={(values, { setSubmitting }) => {
-         setTimeout(() => {
-           alert(JSON.stringify(values, null, 2))
-           setSubmitting(false);
-         }, 400)
-       }}
-     >
-       {({ isSubmitting }) => (
-         <Form>
-            <img src={logo} alt="" className="form-control"/>
-            <label htmlFor="floatingInput">Логин</label>
-            <Field className="form-control" type="email" name="email" autoComplete="on"/>
-            <ErrorMessage name="email" component="div" />
-            <label htmlFor="floatingInput">Пароль</label>
-            <Field className="form-control" type="password" name="password" autoComplete="on"/>
-            <ErrorMessage name="password" component="div" />
-            <button className="mt-2 w-100 btn btn-lg btn-primary" type="submit" disabled={isSubmitting}>
-              Войти
-            </button>
-            <p>
-              <a href="/registration" className="float-start">Регистрация</a>    
-              <a href="/forgot_password" className="float-end">Забыли пароль?</a>
-            </p>
-         </Form>
-       )}
-     </Formik>
-   </div>
+        <form>
+          <img src={logo} alt="" className="form-control"/>
+          <label htmlFor="floatingInput">Логин</label>
+          <input
+            className="form-control"
+            type="email"
+            name="email"
+            autoComplete="on"
+            value={state.login}
+            onChange={e => setState({...state, login: e.target.value})}
+          />
+          <label htmlFor="floatingInput">Пароль</label>
+          <input className="form-control" type="password" name="password" value={{...state, password: e.target.value}}/>
+          <button className="mt-2 w-100 btn btn-lg btn-primary" type="submit" disabled={false}>
+            Войти
+          </button>
+          <p>
+            <a href="/registration" className="float-start">Регистрация</a>
+            <a href="/forgot_password" className="float-end">Забыли пароль?</a>
+          </p>
+        </form>
+
+      </div>
     </div>
   );
 }
